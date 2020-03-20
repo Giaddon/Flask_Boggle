0a72549d182cf530c2fb1c76cba8973faf086645
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-03-20 12:38:03 -0700

    Visible board

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "Shhhhh"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
+
+@app.route("/")
+def home():
+    
+    board = boggle_game.make_board() #this needs to go into a session
+
+    print("\n\nBoard", board, "\n\n")
+
+    return render_template("board.html", board = board)
