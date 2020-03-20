46a3202d843702dc2efcdc94054fb19573995cfc
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-03-20 16:26:39 -0700

    Some tests!

--- test.py
+++ test.py
@@ -5,6 +5,67 @@ from boggle import Boggle
 
 
 class FlaskTests(TestCase):
+    "Tests for our flask app"
+    def test_boggle_board(self):
+        with app.test_client() as client:
+            resp = client.get("/")
+            html = resp.get_data(as_text=True)
 
-    # TODO -- write tests for every view function / feature!
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<table class="boggle-board">', html)
+    
+    def test_boggle_check_correct(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['board'] = [
+                    ['W', 'M', 'D', 'L', 'R'], 
+                    ['I', 'X', 'I', 'V', 'H'], 
+                    ['N', 'R', 'A', 'G', 'I'], 
+                    ['B', 'K', 'R', 'O', 'E'], 
+                    ['G', 'P', 'N', 'S', 'V']
+                    ] 
+
+            resp = client.post('/check/',
+                        json={'guess': 'win'})
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Cool! Good job Guy", html)
+    
+    def test_boggle_check_invalid(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['board'] = [
+                    ['W', 'M', 'D', 'L', 'R'], 
+                    ['I', 'X', 'I', 'V', 'H'], 
+                    ['N', 'R', 'A', 'G', 'I'], 
+                    ['B', 'K', 'R', 'O', 'E'], 
+                    ['G', 'P', 'N', 'S', 'V']
+                    ] 
+
+            resp = client.post('/check/',
+                        json={'guess': 'tttt'})
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("This isn't a word", html)
+        
+    def test_boggle_check_not_there(self):
+        with app.test_client() as client:
+            with client.session_transaction() as change_session:
+                change_session['board'] = [
+                    ['W', 'M', 'D', 'L', 'R'], 
+                    ['I', 'X', 'I', 'V', 'H'], 
+                    ['N', 'R', 'A', 'G', 'I'], 
+                    ['B', 'K', 'R', 'O', 'E'], 
+                    ['G', 'P', 'N', 'S', 'V']
+                    ] 
+
+            resp = client.post('/check/',
+                        json={'guess': 'triumphant'})
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Word isn't on the board!", html)
+
 
